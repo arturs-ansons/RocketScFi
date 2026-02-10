64322a828144cb58685f386050a2e0f3f9e9ef6f
Arnas Kičas <arnas.kicas@mif.stud.vu.lt>
2026-02-10 12:39:55 +0200

    Trying to create the initial page

---
+++ src/main/java/controller/HomeController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("rocket", "Falcon 9");
+        model.addAttribute("speed", 9000);
+        return "index";
+    }
+}
