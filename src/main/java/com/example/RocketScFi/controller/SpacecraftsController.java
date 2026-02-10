98a5cdabba9c4fdda7457921dc00fb18cdfb2eb7
Diamondtroller <46786192+Diamondtroller@users.noreply.github.com>
2026-02-10 18:30:58 +0200

    Minor changes to services

--- src/main/java/com/example/RocketScFi/controller/SpacecraftsController.java
+++ src/main/java/com/example/RocketScFi/controller/SpacecraftsController.java
@@ -19,7 +19,7 @@ public class SpacecraftsController {
     // GET /spacecrafts -> list page
     @GetMapping
     public String listItems(Model model) {
-        model.addAttribute("spacecrafts", rocketService.getSpacecrafts());
+        model.addAttribute("spacecrafts", rocketService.findAll());
         return "spacecrafts"; // Thymeleaf template: spacecrafts.html
     }
 
@@ -33,7 +33,7 @@ public class SpacecraftsController {
     // POST /spacecrafts -> save new spacecraft
     @PostMapping
     public String save(@ModelAttribute Spacecraft spacecraft) {
-        rocketService.createSpacecraft(
+        rocketService.save(
                 spacecraft.getName(),
                 spacecraft.getMass(),
                 spacecraft.getManufacturer()
