f17b1f80a26dfe00eca877409eae338dcc5db37a
Arnas Kičas <arnas.kicas@mif.stud.vu.lt>
2026-02-10 12:42:01 +0200

    Pulled changes where the model was added

--- src/main/java/com/example/RocketScFi/controller/HomeController.java
+++ src/main/java/com/example/RocketScFi/controller/HomeController.java
@@ -1,5 +1,6 @@
-package controller;
+package com.example.RocketScFi.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
