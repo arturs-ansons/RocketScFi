2887f5f344d7f045ec4a8b2ab7b30d837ce72bbc
ansonsarturs <ansonsarturs@gmail.com>
2026-02-10 11:21:57 +0200

    v1

---
+++ src/main/java/com/example/RocketScFi/RocketScFiApplication.java
@@ -0,0 +1,13 @@
+package com.example.RocketScFi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketScFiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocketScFiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RocketScFi/RocketScFiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RocketScFi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketScFiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
