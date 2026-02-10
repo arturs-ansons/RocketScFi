8cb0f95cdb32b2db6a457671e0a63829c08e9809
Diamondtroller <46786192+Diamondtroller@users.noreply.github.com>
2026-02-10 13:07:49 +0200

    Service layer start

---
+++ src/main/java/com/example/RocketScFi/service/RocketService.java
@@ -0,0 +1,30 @@
+package com.example.RocketScFi.service;
+
+import com.example.RocketScFi.model.Crew;
+import com.example.RocketScFi.model.Mission;
+import com.example.RocketScFi.model.Person;
+import com.example.RocketScFi.model.Spacecraft;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class RocketService {
+    SortedMap<Long, Spacecraft> spacecrafts = new TreeMap<>();
+
+
+    public Spacecraft createSpacecraft(String name, double mass, String manufacturer) {
+        Spacecraft spacecraft = new Spacecraft();
+        long id;
+        try {
+            id = spacecrafts.lastKey() + 1;
+        } catch(NoSuchElementException e) {
+            id = 0;
+        }
+        spacecraft.setId(id);
+        return spacecrafts.put(id, spacecraft);
+    }
+
+    public Spacecraft getSpacecrafts(long id);
+    public Spacecraft getSpacecrafts();
+}
