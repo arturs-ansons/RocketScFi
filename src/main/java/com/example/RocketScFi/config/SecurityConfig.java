08091a72129f27acb0729264b8d5e919de113231
Diamondtroller <46786192+Diamondtroller@users.noreply.github.com>
2026-02-11 13:32:23 +0200

    H2 working

--- src/main/java/com/example/RocketScFi/config/SecurityConfig.java
+++ src/main/java/com/example/RocketScFi/config/SecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -43,6 +43,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/spacecrafts/new").hasRole("ADMIN")              // admin only for creating
                         .requestMatchers("/spacecrafts").hasAnyRole("ADMIN", "USER")       // list accessible to logged-in users
